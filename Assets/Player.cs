42424eef222e3e84f81d59e779d2fea2431e11af
Gaetz <gaetz@rufflerim.com>
2018-05-11 12:35:55 +0200

    Ship control

---
+++ Assets/Player.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour {
+
+	[Tooltip("In m/s")][SerializeField] float speed = 17.0f;
+	[SerializeField] float xRange = 8.5f;
+	[SerializeField] float yRange = 4.5f;
+	[SerializeField] float positionPitchFactor = -5f;
+	[SerializeField] float controlPitchFactor = -20f;
+	[SerializeField] float positionYawFactor = -5f;
+	[SerializeField] float controlRollFactor = -20f;
+
+	float xThrow, yThrow;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+		yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+		UpdatePosition();
+		UpdateRotation();
+	}
+
+	void UpdatePosition() {
+		float xOffset = xThrow * speed * Time.deltaTime;
+		float yOffset = yThrow * speed * Time.deltaTime;
+		Vector3 position = transform.localPosition;
+		transform.localPosition = new Vector3(Mathf.Clamp(position.x + xOffset, -xRange, xRange), Mathf.Clamp(position.y + yOffset, -yRange, yRange), position.z);
+	}
+
+	void UpdateRotation() {
+		float pitch = transform.localPosition.y * positionPitchFactor + yThrow * controlPitchFactor;
+		float yaw = transform.localPosition.x * positionYawFactor;
+		float roll = xThrow * controlRollFactor;
+		transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
+	}
+}
