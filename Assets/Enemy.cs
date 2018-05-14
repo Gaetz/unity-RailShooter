066ef60f95a08f75713530c13299a6f78a4a9ab5
Gaetz <gaetz@rufflerim.com>
2018-05-14 12:03:24 +0200

    Enemies add their own colliders

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -4,15 +4,13 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour {
 
-	// Use this for initialization
+	BoxCollider boxCollider;
+
 	void Start () {
-		
+		boxCollider = gameObject.AddComponent<BoxCollider>();
+		boxCollider.isTrigger = false;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
 
 	void OnParticleCollision(GameObject other) {
 		Destroy(gameObject);
