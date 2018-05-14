9cd14422cd948a356e4fb06345d91e15c1746b59
Gaetz <gaetz@rufflerim.com>
2018-05-14 15:00:30 +0200

    Score display and increase

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -6,16 +6,21 @@ public class Enemy : MonoBehaviour {
 
 	[SerializeField] Transform parent;
 	[SerializeField] GameObject deathFx;
+	[SerializeField] int scoreByHit = 12;
+
 	BoxCollider boxCollider;
+	ScoreHandler score;
 
 	void Start () {
 		boxCollider = gameObject.AddComponent<BoxCollider>();
 		boxCollider.isTrigger = false;
+		score = FindObjectOfType<ScoreHandler>();
 	}
 
 
 	void OnParticleCollision(GameObject other) {
 		GameObject fx = Instantiate(deathFx, transform.position, Quaternion.identity);
+		score.OnScoreHit(scoreByHit);
 		fx.transform.parent = parent;
 		Destroy(gameObject);
 		Destroy(fx, 2f);
---
+++ Assets/ScoreHandler.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreHandler : MonoBehaviour {
+	int score = 0;
+	Text scoreText;
+
+	void Start () {
+		scoreText = GetComponent<Text>();
+		UpdateScore();
+	}
+	
+	public void OnScoreHit (int scoreByHit) {
+		score += scoreByHit;
+		UpdateScore();
+	}
+
+	void UpdateScore() {
+		scoreText.text = score.ToString();
+	}
+}
